1e1782f2c9ee2d0ed37bb35e30fd9166aa594fc2
Mykola Zubets <kolya.zubets@gmail.com>
2021-04-24 11:19:42 -0700

    Lab2 Done

---
+++ MobDev/DiagramView.swift
@@ -0,0 +1,20 @@
+//
+//  DiagramView.swift
+//  MobDev
+//
+//  Created by Mykola Zubets on 24.04.2021.
+//
+
+import UIKit
+
+class DiagramView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MobDev/DrawView.swift
@@ -0,0 +1,20 @@
+//
+//  DrawView.swift
+//  MobDev
+//
+//  Created by Mykola Zubets on 24.04.2021.
+//
+
+import UIKit
+
+class DrawView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
