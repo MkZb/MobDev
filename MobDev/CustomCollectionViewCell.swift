99306af17b8387ee7d75c8319bad528dc55d4e50
Mykola Zubets <kolya.zubets@gmail.com>
2021-05-08 15:44:53 -0700

    Lab 5 done

---
+++ MobDev/CustomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewCell.swift
+//  MobDev
+//
+//  Created by Mykola Zubets on 08.05.2021.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MobDev/CustomCollectionViewLayout.swift
@@ -0,0 +1,12 @@
+//
+//  CustomCollectionViewLayout.swift
+//  MobDev
+//
+//  Created by Mykola Zubets on 08.05.2021.
+//
+
+import UIKit
+
+class CustomCollectionViewLayout: UICollectionViewLayout {
+
+}
